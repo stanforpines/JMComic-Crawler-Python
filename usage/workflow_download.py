a969e17d3a164457df3171ed8e31d877c6ee6875
stanforpines <151711224+stanforpines@users.noreply.github.com>
2024-10-29 19:46:23 +0800

    Update workflow_download.py

--- usage/workflow_download.py
+++ usage/workflow_download.py
@@ -2,7 +2,7 @@ from jmcomic import *
 from jmcomic.cl import JmcomicUI
 
 # 下方填入你要下载的本子的id，一行一个，每行的首尾可以有空白字符
-jm_albums = '''
+jm_albums = 422866'''  
 
 
 
@@ -35,7 +35,7 @@ def get_id_set(env_name, given):
         given,
         (env(env_name, '')).replace('-', '\n'),
     ]:
-        aid_set.update(str_to_set(text))
+        aid_set.update(str_to_set(text))  
 
     return aid_set
 
